d10311b76b06b7cb6fca08737e21c8f4caf99247
NNNNH3 <131462555+NNNNH3@users.noreply.github.com>
2023-11-25 09:33:30 +0500

    Delete hack2023uunt directory

--- hack2023uunt/index2.js
+++
@@ -1,9 +0,0 @@
-document.getElementById("show-register-modal").addEventListener("click",function(){
-    document.getElementById("modal-reg").classList.add("open")
-    
-})
-
-
-document.getElementById("close-popupp-reg").addEventListener("click",function(){
-    document.getElementById("modal-reg").classList.remove("open")
-})
