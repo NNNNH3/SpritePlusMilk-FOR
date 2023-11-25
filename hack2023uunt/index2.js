1de8f101333ab999fde8d6016df979945b591e3c
NNNNH3 <131462555+NNNNH3@users.noreply.github.com>
2023-11-25 09:32:45 +0500

    Add files via upload

---
+++ hack2023uunt/index2.js
@@ -0,0 +1,9 @@
+document.getElementById("show-register-modal").addEventListener("click",function(){
+    document.getElementById("modal-reg").classList.add("open")
+    
+})
+
+
+document.getElementById("close-popupp-reg").addEventListener("click",function(){
+    document.getElementById("modal-reg").classList.remove("open")
+})
